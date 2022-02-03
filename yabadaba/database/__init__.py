b68bafd4467b2dc49e2cf9d8ff2272af80af8fce
Lucas Hale <lucas.hale@nist.gov>
2022-02-03 10:29:30 -0500

    Update to use import_style

--- yabadaba/database/__init__.py
+++ yabadaba/database/__init__.py
@@ -1,6 +1,5 @@
 # coding: utf-8
-# Standard Python libraries
-import sys
+__all__ = ['Database', 'databasemanager', 'load_database']
 
 # Import base Database class
 from .Database import Database
@@ -12,34 +11,7 @@ databasemanager = ModuleManager('Database')
 # Import load_database
 from .load_database import load_database
 
-# Add non-modular components to __all__
-__all__ = ['Database', 'databasemanager', 'load_database']
-
-# Import LocalDatabase
-try:
-    from .LocalDatabase import LocalDatabase
-except Exception as e:
-    databasemanager.failed_styles['local'] = '%s: %s' % sys.exc_info()[:2]
-else:
-    databasemanager.loaded_styles['local'] = LocalDatabase
-    __all__.append('LocalDatabase')
-
-# Import MongoDatabase
-try:
-    from .MongoDatabase import MongoDatabase
-except Exception as e:
-    databasemanager.failed_styles['mongo'] = '%s: %s' % sys.exc_info()[:2]
-else:
-    databasemanager.loaded_styles['mongo'] = MongoDatabase
-    __all__.append('MongoDatabase')
-
-# Import CDCSDatabase
-try:
-    from .CDCSDatabase import CDCSDatabase
-except Exception as e:
-    databasemanager.failed_styles['cdcs'] = '%s: %s' % sys.exc_info()[:2]
-else:
-    databasemanager.loaded_styles['cdcs'] = CDCSDatabase
-    __all__.append('CDCSDatabase')
-
-__all__.sort()
+# Add the modular Database styles
+databasemanager.import_style('local', '.LocalDatabase', __name__)
+databasemanager.import_style('mongo', '.MongoDatabase', __name__)
+databasemanager.import_style('cdcs', '.CDCSDatabase', __name__)
