d0cb42aeae6432cafcdfa84f88e5b948271f659a
Lucas Hale <lucas.hale@nist.gov>
2023-07-27 11:24:25 -0400

    Update for python 3.11

--- yabadaba/__init__.py
+++ yabadaba/__init__.py
@@ -1,17 +1,12 @@
 # coding: utf-8
-__all__ = sorted(['__version__', 'tools', 'settings',
-                 'query', 'load_query', 'querymanager',
-                 'record', 'load_record', 'recordmanager',
-                 'database', 'load_database', 'databasemanager'])
-
 # Standard Python libraries
 from importlib import resources
 
 # Read version from VERSION file
-__version__ = resources.read_text('yabadaba', 'VERSION').strip()
-
-__all__ = ['__version__', 'tools', 'settings', 'record', 'load_record']
-__all__.sort()
+if hasattr(resources, 'files'):
+    __version__ = resources.files('yabadaba').joinpath('VERSION').read_text(encoding='UTF-8')
+else:
+    __version__ = resources.read_text('yabadaba', 'VERSION', encoding='UTF-8').strip()
 
 # Relative imports
 from .UnitConverter import unitconvert
@@ -28,3 +23,9 @@ from .record import recordmanager, load_record
 
 from . import database
 from .database import databasemanager, load_database
+
+__all__ = ['__version__', 'tools', 'settings', 'unitconvert',
+           'query', 'load_query', 'querymanager', 'demo',
+           'record', 'load_record', 'recordmanager',
+           'database', 'load_database', 'databasemanager']
+__all__.sort()
